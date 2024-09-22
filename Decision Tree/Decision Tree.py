979cd0f18b2f77c5e9337f16186f8445624d6abf
coderxguan <3288733739@qq.com>
2024-09-22 17:43:53 +0800

    decision tree

--- Decision Tree/Decision Tree.py
+++ Decision Tree/Decision Tree.py
@@ -1,5 +1,14 @@
+import pickle
+from html.parser import piclose
+
+from fontTools.misc.classifyTools import classify
+from matplotlib.font_manager import  FontProperties
+import matplotlib.pyplot as plt
 import math
 import operator
+
+from matplotlib.pyplot import xticks, yticks
+from sklearn.tree import plot_tree
 
 
 def createDataSet():
@@ -18,7 +27,7 @@ def createDataSet():
             [2, 1, 0, 1, 'yes'],
             [2, 1, 0, 2, 'yes'],
             [2, 0, 0, 0, 'no']]
-    labels = ['年龄', '有工作', '有自己的房子', '信贷情况']             #分类属性
+    labels = ['age', 'have work', 'have house', 'credit status']             #分类属性
     return dataSet, labels
 
 
@@ -110,17 +119,113 @@ def createTree(dataSet, labels, featLabels):
     return myTree
 
 
+# get the number of leaf node of the decision tree
+def getNumLeafs(myTree):
+    numLeafs = 0
+    firstStr = next(iter(myTree))
+    secondDict  = myTree[firstStr]
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__=='dict':
+            numLeafs += getNumLeafs(secondDict[key])
+        else:
+            numLeafs += 1
+    return numLeafs
+
+
+# get the depth of the tree
+def getTreeDepth(myTree):
+    maxDepth = 0
+    firstStr = next(iter(myTree))
+    secondDict = myTree[firstStr]
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__=='dict':
+            thisDepth = 1 + getTreeDepth(secondDict[key])
+        else: thisDepth = 1
+        if thisDepth > maxDepth: maxDepth = thisDepth
+    return maxDepth
+
+# portray  node
+def plotNode(nodeTxt, centerPt, parentPt, nodeType):
+    arrow_args = dict(arrowstyle="<-")  # set arrow format
+    createPlot.ax1.annotate(nodeTxt, xy=parentPt, xycoords='axes fraction',
+                            xytext=centerPt, textcoords='axes fraction',
+                            va='center', ha='center', bbox=nodeType, arrowprops=arrow_args)
+
+
+def plotMidText(cntrPt, parentPt, txtString):
+    xMid = (parentPt[0] - cntrPt[0]) / 2.0 + cntrPt[0]  # 计算标注位置
+    yMid = (parentPt[1] - cntrPt[1]) / 2.0 + cntrPt[1]
+    createPlot.ax1.text(xMid, yMid, txtString, va="center", ha="center", rotation=30)
 
 
 
 
+def plotTree(myTree, parentPt, nodeTxt):
+    decisionNode = dict(boxstyle="sawtooth", fc="0.8")  # 设置结点格式
+    leafNode = dict(boxstyle="round4", fc="0.8")  # 设置叶结点格式
+    numLeafs = getNumLeafs(myTree)  # 获取决策树叶结点数目，决定了树的宽度
+    depth = getTreeDepth(myTree)  # 获取决策树层数
+    firstStr = next(iter(myTree))  # 下个字典
+    cntrPt = (plotTree.xOff + (1.0 + float(numLeafs)) / 2.0 / plotTree.totalW, plotTree.yOff)  # 中心位置
+    plotMidText(cntrPt, parentPt, nodeTxt)  # 标注有向边属性值
+    plotNode(firstStr, cntrPt, parentPt, decisionNode)  # 绘制结点
+    secondDict = myTree[firstStr]  # 下一个字典，也就是继续绘制子结点
+    plotTree.yOff = plotTree.yOff - 1.0 / plotTree.totalD  # y偏移
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__ == 'dict':  # 测试该结点是否为字典，如果不是字典，代表此结点为叶子结点
+            plotTree(secondDict[key], cntrPt, str(key))  # 不是叶结点，递归调用继续绘制
+        else:  # 如果是叶结点，绘制叶结点，并标注有向边属性值
+            plotTree.xOff = plotTree.xOff + 1.0 / plotTree.totalW
+            plotNode(secondDict[key], (plotTree.xOff, plotTree.yOff), cntrPt, leafNode)
+            plotMidText((plotTree.xOff, plotTree.yOff), cntrPt, str(key))
+    plotTree.yOff = plotTree.yOff + 1.0 / plotTree.totalD
+
+# create portray panel
+def createPlot(inTree):
+    fig = plt.figure(1, facecolor='white')
+    fig.clf()
+    axprops = dict(xticks=[], yticks=[])
+    createPlot.ax1 = plt.subplot(111, frameon=False, **axprops)
+    plotTree.totalW = float(getNumLeafs(inTree))
+    plotTree.totalD = float(getTreeDepth(inTree))
+    plotTree.xOff = -0.5/plotTree.totalW; plotTree.yOff = 1.0;                                #x偏移
+    plotTree(inTree, (0.5,1.0), '')                                                            #绘制决策树
+    plt.show()
 
 
+def classify(inputTree, featLabels, testVec):
+    firstStr = next(iter(inputTree))                                                        #获取决策树结点
+    secondDict = inputTree[firstStr]                                                        #下一个字典
+    featIndex = featLabels.index(firstStr)
+    for key in secondDict.keys():
+        if testVec[featIndex] == key:
+            if type(secondDict[key]).__name__ == 'dict':
+                classLabel = classify(secondDict[key], featLabels, testVec)
+            else: classLabel = secondDict[key]
+    return classLabel
+
+
+def storeTree(inputTree, filename):
+    with open(filename, 'wb') as fw:
+        pickle.dump(inputTree, fw)
+
+
+def grapTree(filename):
+    fr = open(filename, 'rb')
+    return pickle.load(fr)
 
 if __name__ == '__main__':
     dataSet, labels = createDataSet()
     featLabels = []
     myTree = createTree(dataSet, labels, featLabels)
+    storeTree(myTree, 'classifierStorage.txt')
+    testVec = [0,1]
+    result = classify(myTree, featLabels, testVec)
+    if result == 'yes':
+        print("放贷")
+    if result == 'no':
+        print("不房贷")
     print(myTree)
+    createPlot(myTree)
 
     pass
